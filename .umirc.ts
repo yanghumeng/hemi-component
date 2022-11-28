36ccd2e8ab8e3deb0af500f60db266997730e72c
hemi <1830720944@qq.com>
2022-11-28 17:00:20 +0800

    feat: 修改基础库环境

--- .umirc.ts
+++ .umirc.ts
@@ -25,14 +25,14 @@ export default defineConfig({
   // 引入被 external 库的 scripts
   // 区分 development 和 production，使用不同的产物
   scripts:
-    process.env.NODE_ENV === 'dev'
+    process.env.NODE_ENV === 'production'
       ? [
-          'https://gw.alipayobjects.com/os/lib/react/16.13.1/umd/react.development.js',
-          'https://gw.alipayobjects.com/os/lib/react-dom/16.13.1/umd/react-dom.development.js',
-        ]
-      : [
           'https://gw.alipayobjects.com/os/lib/react/16.13.1/umd/react.production.min.js',
           'https://gw.alipayobjects.com/os/lib/react-dom/16.13.1/umd/react-dom.production.min.js',
+        ]
+      : [
+          'https://gw.alipayobjects.com/os/lib/react/16.13.1/umd/react.development.js',
+          'https://gw.alipayobjects.com/os/lib/react-dom/16.13.1/umd/react-dom.development.js',
         ],
   // 配置 external,表示哪些模块可以不被打包
   externals: {
