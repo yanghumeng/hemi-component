aef73ebd9028e9a9c7215a2ec91e1b62b892e536
hemi <1830720944@qq.com>
2022-12-20 17:12:12 +0800

    feat: father配置

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -1,4 +1,30 @@
 export default {
   // more father 4 config: https://github.com/umijs/father-next/blob/master/docs/config.md
-  esm: {},
+  cjs: { type: 'babel', lazy: true },
+  esm: {
+    type: 'babel',
+    importLibToEs: true,
+  },
+  cssModules: true,
+  extraBabelPlugins: [
+    [
+      'import',
+      {
+        libraryName: 'antd',
+        libraryDirectory: 'es',
+        style: true,
+      },
+    ],
+    [
+      require.resolve('babel-plugin-module-resolver'),
+      {
+        root: ['./'],
+        extensions: ['.js', '.jsx', '.tsx'],
+        alias: {
+          '@basics@': '@dm-component-next/basics/es',
+        },
+      },
+    ],
+  ],
+  pkgs: ['utils', 'basics'],
 };
