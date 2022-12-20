c8395381a4e667867a378378ce1d639b4c4a1dde
hemi <1830720944@qq.com>
2022-12-20 22:36:25 +0800

    feat: build

---
+++ packages/utils/lib/utils.d.ts
@@ -0,0 +1,13 @@
+/**
+ *获取逻辑的参数
+ * @param url 路径
+ * @param key 关键字
+ * @returns
+ */
+export declare function getUrlParams(url: string, key?: string): any;
+/**
+ * 大写金额
+ * @param num 值
+ * @returns
+ */
+export declare function digitUppercase(num: string | number): string;
