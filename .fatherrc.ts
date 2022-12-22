788f9a3beae318b671a72969d0774af58546147a
hemi <1830720944@qq.com>
2022-12-22 23:21:01 +0800

    feat: none

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -1,27 +1,27 @@
 export default {
   // more father 4 config: https://github.com/umijs/father-next/blob/master/docs/config.md
-  // cjs: { transformer: 'babel' },
-  // esm: {
-  //   transformer: 'babel',
-  // },
-  // extraBabelPlugins: [
-  //   [
-  //     'import',
-  //     {
-  //       libraryName: 'antd',
-  //       libraryDirectory: 'es',
-  //       style: true,
-  //     },
-  //   ],
-  //   [
-  //     require.resolve('babel-plugin-module-resolver'),
-  //     {
-  //       root: ['./'],
-  //       extensions: ['.js', '.jsx', '.tsx'],
-  //       alias: {
-  //         '@basics@': '@hemi-component/basics/es',
-  //       },
-  //     },
-  //   ],
-  // ],
+  cjs: { transformer: 'babel' },
+  esm: {
+    transformer: 'babel',
+  },
+  extraBabelPlugins: [
+    [
+      'import',
+      {
+        libraryName: 'antd',
+        libraryDirectory: 'es',
+        style: true,
+      },
+    ],
+    [
+      require.resolve('babel-plugin-module-resolver'),
+      {
+        root: ['./'],
+        extensions: ['.js', '.jsx', '.tsx'],
+        alias: {
+          '@basics@': '@hemi-component/basics/es',
+        },
+      },
+    ],
+  ],
 };
