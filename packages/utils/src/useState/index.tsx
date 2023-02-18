d9c82e8b26ba3175a5c079cdb2ee9c58776f3de7
hemi <1830720944@qq.com>
2023-02-18 17:29:16 +0800

    feat: 删除usestaue函数初始化

--- packages/utils/src/useState/index.tsx
+++
@@ -1,8 +0,0 @@
-import React, { useState } from 'react';
-
-const index = (initValue: any, callback: any) => {
-  const [first, setfirst] = useState(initValue);
-  return <div></div>;
-};
-
-export default index;
